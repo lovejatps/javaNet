02f33aed09d2795a42e823a5986f02d02f91c791
huxn <huxn@sina.com>
2016-08-23 18:02:53 +0800

    java模拟表单上传图片

---
+++ src/javaNet/client/Client_Post.java
@@ -0,0 +1,121 @@
+package javaNet.client;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * @author huxiaoning
+ * @ClassName: Client_Post  
+ * @author A18ccms a18ccms_gmail_com  
+ * @date 2016年8月23日 上午10:27:23
+ */
+public class Client_Post {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String url ="http://127.0.0.1:8080/server/comment/save";
+		String file1 ="/Users/huxiaoning/Downloads/750x562-3.jpg";
+		String file2 ="/Users/huxiaoning/Downloads/750x562－1.JPG";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("userId", "efc132507b8445f585ba3d58071a21a0");
+		params.put("sceneryId", "2");
+		params.put("commentTags", "2,3");
+		params.put("starNum",4);
+		params.put("content", "555555555555");
+		Map<String,File>  files = new HashMap<>();
+		files.put("file1", new File(file1));
+//		files.put("file2", new File(file2));
+		post(url,params,files);
+		
+		
+
+	}
+	
+	public static String post(String url, Map<String, Object> params, Map<String, File> files)
+            throws IOException {
+        String BOUNDARY = java.util.UUID.randomUUID().toString();
+        String PREFIX = "--", LINEND = "\r\n";
+        String MULTIPART_FROM_DATA = "multipart/form-data";
+        String CHARSET = "UTF-8";
+
+
+        URL uri = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
+        conn.setReadTimeout(10 * 1000); // 缓存的最长时间
+        conn.setDoInput(true);// 允许输入
+        conn.setDoOutput(true);// 允许输出
+        conn.setUseCaches(false); // 不允许使用缓存
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("connection", "keep-alive");
+        conn.setRequestProperty("Charsert", "UTF-8");
+        conn.setRequestProperty("Content-Type", MULTIPART_FROM_DATA + ";boundary=" + BOUNDARY);
+
+
+        // 首先组拼文本类型的参数
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Object> entry : params.entrySet()) {
+            sb.append(PREFIX);
+            sb.append(BOUNDARY);
+            sb.append(LINEND);
+            sb.append("Content-Disposition: form-data; name=\"" + entry.getKey() + "\"" + LINEND);
+            sb.append("Content-Type: text/plain; charset=" + CHARSET + LINEND);
+            sb.append("Content-Transfer-Encoding: 8bit" + LINEND);
+            sb.append(LINEND);
+            sb.append(entry.getValue());
+            sb.append(LINEND);
+        }
+
+
+        DataOutputStream outStream = new DataOutputStream(conn.getOutputStream());
+        outStream.write(sb.toString().getBytes());
+        // 发送文件数据
+        if (files != null)
+            for (Map.Entry<String, File> file : files.entrySet()) {
+                StringBuilder sb1 = new StringBuilder();
+                sb1.append(PREFIX);
+                sb1.append(BOUNDARY);
+                sb1.append(LINEND);
+                sb1.append("Content-Disposition: form-data; name=\"uploadfile\"; filename=\""
+                        + file.getValue().getName() + "\"" + LINEND);
+                sb1.append("Content-Type: application/octet-stream; charset=" + CHARSET + LINEND);
+                sb1.append(LINEND);
+                outStream.write(sb1.toString().getBytes(CHARSET));
+
+
+                InputStream is = new FileInputStream(file.getValue());
+                byte[] buffer = new byte[2048];
+                int len = 0;
+                while ((len = is.read(buffer)) != -1) {
+                    outStream.write(buffer, 0, len);
+                }
+                is.close();
+                outStream.write(LINEND.getBytes(CHARSET));
+            }
+        // 请求结束标志
+        byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINEND).getBytes(CHARSET);
+        outStream.write(end_data);
+        outStream.flush();
+        // 得到响应码
+        int res = conn.getResponseCode();
+        InputStream in = conn.getInputStream();
+        StringBuilder sb2 = new StringBuilder();
+        if (res == 200) {
+            int ch;
+            while ((ch = in.read()) != -1) {
+                sb2.append((char) ch);
+            }
+        }
+        outStream.close();
+        conn.disconnect();
+        return sb2.toString();
+	}
+	
+}
---
+++ src/javaNet/client/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * <p>Title:package-info</p>
+ * <p>Description:</p>
+ * <p>Company:</p>
+ * @aouthor huxiaoning
+ * @date 2016年8月23日 上午10:26:34
+ */
+/**
+ * @author huxiaoning
+ * @ClassName: package-info  
+ * @author A18ccms a18ccms_gmail_com  
+ * @date 2016年8月23日 上午10:26:34  
+ */
+package javaNet.client;
